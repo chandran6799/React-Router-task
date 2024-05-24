b737d4350f666078c9239cecda08f6a76c2e6d3f
chandran6799 <chandranak06071999@gmail.comgit config --global user.email chandranak06071999@gmail.com>
2024-05-24 06:25:19 -0700

    Router Commit

---
+++ src/Components/CyberSecurity.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "./Card";
+
+const CyberSecurity = ({ data }) => {
+  const cyber = data.filter((item) => item.head === "Cyber security");
+  return (
+    <div className="container">
+      <div className="row py-5 d-flex justify-content-center">
+        {/* <h1>Cyber</h1> */}
+        {cyber.map((item, index) => {
+          return (
+            <>
+              <Card item={item} index={index} />
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default CyberSecurity;
